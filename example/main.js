7e7731edf176b69e3b44ae7563e05fa06239848b
Abdul Rehman <mehars.6925@gmail.com>
2021-11-01 06:12:56 +0500

    added tracking example

--- example/main.js
+++ example/main.js
@@ -1,3 +1,7 @@
+// Normal Mode
 (async () => {
     console.log(await require('../index')());
-})()
+})();
+
+// Tracker Mode
+setInterval(async () => console.log(await require('../index')()), 10000);
