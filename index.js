beadbde0e32a0841eebb2a832c27531d250918b2
Abdul Rehman <mehars.6925@gmail.com>
2021-11-01 05:19:17 +0500

    configured to run osascripts on darwin

---
+++ index.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = () => {
+    return new Promise((resolve, reject) => {
+        let returnable;
+        if (process.platform === 'darwin') {
+            const osascript = require('node-osascript');
+            osascript.executeFile(
+                path.join(__dirname, './resources/get-foreground-window-title.osa'), 
+                (error, data, raw) => {
+                    if (error) reject(error);
+                    returnable = {
+                        processName: data[0],
+                        processTitle: data[1],
+                        filePath: data[2] != 'missing value' ? data[2] : null,
+                        fileName: data[2] != 'missing value' ? data[2].split('/').pop() : null
+                    }
+                    osascript.executeFile(
+                        path.join(__dirname, './resources/get-mouse-location.osa'),
+                        (error, data, raw) => {
+                            if (error) reject(error);
+                            returnable.mouseX = Math.ceil(data[0]),
+                            returnable.mouseY = Math.ceil(data[1])
+                            resolve(returnable);
+                        }
+                    )
+                }
+            );
+        } else {
+            reject('Platform not supported');
+        }
+    });
+};
